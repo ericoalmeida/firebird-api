f5bf1a28aa59e824c6ce7bf490a580610ac7596c
Erico Almeida <ericoalmeidamvp@gmail.com>
2020-09-28 08:01:57 -0400

    Added database connection options file

---
+++ src/settings/database.ts
@@ -0,0 +1,12 @@
+const databaseOptions = {
+  host: 'localhost',
+  port: 3050,
+  database: 'database.fdb',
+  user: 'SYSDBA',
+  password: 'masterkey',
+  lowercase_keys: false,
+  role: null,
+  pageSize: 4096,
+};
+
+export default databaseOptions;
